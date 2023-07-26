c90216a55591867c81183ceb9ab864d04ee35fcf
José Tom´ás Otero García <otero.josetomas@gmail.com>
2023-07-26 18:08:36 -0400

    avance bom, introduccion

---
+++ BOM/window.js
@@ -0,0 +1,20 @@
+// console.log(`La ventana de tu navegador mide ${window.innerWidth} px de ancho`);
+
+// let ventana;
+// const abrirVentana = () => {
+//     ventana = window.open('', 'Mi nueva ventana', 'width=500, height=500');
+//     ventana.document.write('<h1>Hola, escribiendo en la nueva ventana</h1>');
+// }
+
+// const cerrarVentana = () => {
+//     ventana.close();
+// }
+
+
+// screen object
+// console.log('Ancho de pantalla:', window.screen.width);
+// console.log('Ancho de pantalla:', window.screen.height);
+
+// console.log('Ancho de pantalla sin barra de windows:', window.screen.availWidth);
+// console.log('Ancho de pantalla sin barra de windows:', window.screen.availHeight);
+
