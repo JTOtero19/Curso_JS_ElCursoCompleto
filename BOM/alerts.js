1b942d5d28dc9d0c3f3fa9a38815e5950896d856
José Tom´ás Otero García <otero.josetomas@gmail.com>
2023-07-27 15:57:24 -0400

    avance bom, alertas

---
+++ BOM/alerts.js
@@ -0,0 +1,13 @@
+// const ingresar = () => {
+//     const accesoPermitido = confirm("Eri gay?");
+//     if(accesoPermitido){
+//         alert("Bienvenido al club!");
+//     } else {
+//         alert('Asi se hace!');
+//     }
+// }
+
+const ingresar = () => {
+    const respuesta = prompt('Escribe tu respuesta');
+    alert(`Tu respuesta fue: ${respuesta}`);
+};
