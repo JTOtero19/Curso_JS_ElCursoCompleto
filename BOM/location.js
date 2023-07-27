b0471b6d0ccb8ea2abf7a4f3d9d7ed6030805ed5
José Tom´ás Otero García <otero.josetomas@gmail.com>
2023-07-27 14:00:15 -0400

    avance bom, navigator y history

---
+++ BOM/location.js
@@ -0,0 +1,14 @@
+console.log(window.location.href);
+
+
+// const cargarDocumento= () => {
+//     location.assign("https://www.google.com");
+// }
+
+// const regresar = () => {
+//     history.back();
+// }
+
+// const regresar = () => {
+//     history.forward();
+// }
