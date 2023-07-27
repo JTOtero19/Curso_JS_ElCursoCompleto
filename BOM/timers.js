33e136a3d7c08e543b179c18a4fb0454e4ca356c
José Tom´ás Otero García <otero.josetomas@gmail.com>
2023-07-27 16:31:25 -0400

    avance bom, timers

---
+++ BOM/timers.js
@@ -0,0 +1,32 @@
+// const saludo = () => {
+//   console.log("Hola Mundo");
+// };
+
+// let id; // variable global.
+// const iniciar = () => {
+//   console.log('Iniciando...');
+//   id = setInterval(saludo, 1500); // esta es una variable local.
+// }
+
+// const detener = () => {
+//   console.log('Deteniendo...');
+//   clearTimeout(id);
+//   console.log('Detenido');
+// }
+
+
+// setinterval() -> ejecuta una función cada cierto tiempo.
+
+let cuenta = 0;
+let id; // variable global.
+
+const iniciar = () => {
+  id = setInterval(() => {
+    console.log(cuenta++);
+  }, 1000);
+}
+
+const detener = () => {
+    console.log('Deteniendo...');
+    clearInterval(id);
+}
