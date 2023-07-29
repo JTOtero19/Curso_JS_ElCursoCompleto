0e0751b4cf56439abb65aad2dcb2254796386962
José Tom´ás Otero García <otero.josetomas@gmail.com>
2023-07-29 14:00:53 -0400

    avance, boom cookie 4

--- BOM/cookies.js
+++ BOM/cookies.js
@@ -1,11 +1,29 @@
-const crearCookie = () => {
-    document.cookie = 'nombre=Jorge; expires=1 Jan 2024 01:00:00 UTC';
-}
+// const crearCookie = () => {
+//     document.cookie = 'nombre=Jorge; expires=1 Jan 2024 01:00:00 UTC';
+// }
 
-const iniciarSesion = () => {
-    const usuario = promt('Usuario:');
-    document.cookie = `nombre=${usuario}; expires=1 Jan 2024 01:00:00 UTC`;
-    alert('Sesion iniciada');
-}
+// const iniciarSesion = () => {
+//     const usuario = promt('Usuario:');
+//     document.cookie = `nombre=${usuario}; expires=1 Jan 2024 01:00:00 UTC`;
+//     alert('Sesion iniciada');
+// }
 
-console.log(document.cookie);
+// console.log(document.cookie);
+
+/*
+Forma para obtener el usuario de las cookies
+ */
+let usuario;
+const cookies = document.cookie.split('; ');
+cookies.forEach((cookie) => {
+    const propiedad = cookie.split('=')[0];
+    if(propiedad === 'nombre') {
+        usuario = cookie.split('=')[1];
+    }
+});
+
+if(usuario){
+    console.log(`hola ${usuario}`);
+} else {
+    console.log('No has iniciado sesion');
+};
