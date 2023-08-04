b75c6bd0a78785f64b9032016d7c21bfb862d9d4
José Tom´ás Otero García <otero.josetomas@gmail.com>
2023-08-04 13:17:01 -0400

    avance dom, elementos

---
+++ DOM/modificandoElementos.js
@@ -0,0 +1,10 @@
+const primeraCaja = document.querySelector('.caja');
+// primeraCaja.innerHTML = 'Hola!';
+
+// primeraCaja.id = 'nuevo-id';
+
+// primeraCaja.setAttribute('class', 'caja-activa');
+
+const contenedor2Caja1 = document.querySelector('#contenedor2 .caja');
+contenedor2Caja1.Style.background = '#000';
+contenedor2Caja1.style.color = '#fff';
---
+++ DOM/obtenerElementos.js
@@ -0,0 +1,28 @@
+/*
+get element bi id
+ */
+// const contenedor1 = document.getElementById('contenedor1');
+// console.log(contenedor1);
+
+// console.log(contenedor1.children);
+
+
+// console.log(contenedor1.parentElement)
+
+// const divs = document.getElementsByTagName('div');
+// console.log(divs);
+// console.log(`Tenemos ${divs.length} divs`);
+
+
+// const contenedores = document.getElementsByClassName('contenedor');
+// console.log(contenedores);
+
+// const caja = document.querySelectorAll('#contenedor1 .caja');
+// console.log(caja);
+
+// const ultimaCaja = document.querySelector('#contenedor2 .caja:last-child');
+// console.log(ultimaCaja);
+// console.log(ultimaCaja.closest('.contenedor-principal'));
+
+const contenedor2 = document.getElementById('contenedor2');
+console.log(contenedor2.querySelector('.caja'));
