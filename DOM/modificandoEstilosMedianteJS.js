8cbd2091947950819e4fbe4a4cb1d1b485731db2
José Tom´ás Otero García <otero.josetomas@gmail.com>
2023-08-06 16:25:47 -0400

    avance dom. modificando js listo

--- DOM/modificandoEstilosMedianteJS.js
+++ DOM/modificandoEstilosMedianteJS.js
@@ -1,3 +1,22 @@
 const ultimaCaja = document.querySelector('#contenedor2 .caja:last-child');
 ultimaCaja.style.background = '#000';
 ultimaCaja.style.color = '#fff';
+
+const cajas = document.querySelectorAll('.caja');
+let tamanio = 24;
+
+
+const incrementarFuente = () => {
+    cajas.forEach((caja) => {
+        caja.style.fontSize = `${tamanio + 1}px`;
+        tamanio++;
+    })
+}
+
+
+const disminuirFuente = () => {
+  cajas.forEach((caja) => {
+      caja.style.fontSize = `${tamanio - 1}px`;
+      tamanio--;
+  })
+}
