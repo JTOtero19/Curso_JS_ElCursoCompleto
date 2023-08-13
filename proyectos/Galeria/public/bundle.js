bb35103efbf8eff385f16bd7c86b2fa2ae2514cb
José Tom´ás Otero García <otero.josetomas@gmail.com>
2023-08-13 15:33:55 -0400

    avance categorias con dinamismo galeria

--- proyectos/Galeria/public/bundle.js
+++ proyectos/Galeria/public/bundle.js
@@ -450,8 +450,22 @@ var dataCategorias = {
 };
 
 const { categorias } = dataCategorias;
-document.getElementById('categorias');
+const contenedorCategorias = document.getElementById('categorias');
 
 categorias.forEach((categoria)=>{
-  console.log(categoria);
+  const nuevaCategoria = document.createElement('a');
+  const plantilla = `
+  <img class="categoria__img" src="${categoria.imagenPortada}" alt="" />
+						<div class="categoria__datos">
+							<p class="categoria__nombre">${categoria.nombre}</p>
+							<p class="categoria__numero-fotos">${categoria.numeroFotos} Fotos</p>
+						</div>
+  `;
+
+  nuevaCategoria.innerHTML = plantilla;
+  nuevaCategoria.classList.add('categoria');
+  nuevaCategoria.href = '#';
+  nuevaCategoria.dataset.categoria = categoria.id;
+
+  contenedorCategorias.append(nuevaCategoria);
 });
--- proyectos/Galeria/src/cargarCategorias.js
+++ proyectos/Galeria/src/cargarCategorias.js
@@ -3,5 +3,19 @@ const { categorias } = dataCategorias;
 const contenedorCategorias = document.getElementById('categorias');
 
 categorias.forEach((categoria)=>{
-  console.log(categoria);
+  const nuevaCategoria = document.createElement('a');
+  const plantilla = `
+  <img class="categoria__img" src="${categoria.imagenPortada}" alt="" />
+						<div class="categoria__datos">
+							<p class="categoria__nombre">${categoria.nombre}</p>
+							<p class="categoria__numero-fotos">${categoria.numeroFotos} Fotos</p>
+						</div>
+  `;
+
+  nuevaCategoria.innerHTML = plantilla;
+  nuevaCategoria.classList.add('categoria');
+  nuevaCategoria.href = '#';
+  nuevaCategoria.dataset.categoria = categoria.id;
+
+  contenedorCategorias.append(nuevaCategoria);
 });
