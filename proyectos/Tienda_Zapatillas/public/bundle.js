1ccd5eb2aaf11f95ae60d9e50dce0275a6b611a4
José Tom´ás Otero García <otero.josetomas@gmail.com>
2023-08-21 19:04:35 -0400

    agregando items al carrito, mi carrito

--- proyectos/Tienda_Zapatillas/public/bundle.js
+++ proyectos/Tienda_Zapatillas/public/bundle.js
@@ -62,11 +62,54 @@ const carrito = [];
   Tambien la usaremos en varios bloques.
 */
 const renderCarrito =() => {
-
-  console.log(carrito);
-
   // Este modificador abriara la ventana.
   ventanaCarrito.classList.add('carrito--active');
+
+  // Productos del carrito
+  carrito.forEach((productoCarrito) => {
+    // Creamos una plantilla del codigo HTML.
+    const plantillaProducto = `
+    <div class="carrito__producto-info">
+      <img src="./img/tennis/1.jpg" alt="" class="carrito__thumb" />
+      <div>
+        <p class="carrito__producto-nombre">
+          <span class="carrito__producto-cantidad">1 x </span>Lorem Ipsum Dolot Asimmet
+        </p>
+        <p class="carrito__producto-propiedades">
+          Tamaño:<span>2,5</span> Color:<span>Rojo</span>
+        </p>
+      </div>
+    </div>
+    <div class="carrito__producto-contenedor-precio">
+      <button class="carrito__btn-eliminar-item" data-accion="eliminar-item-carrito">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"
+          />
+        </svg>
+      </button>
+      <p class="carrito__producto-precio">$500.00</p>
+    </div>
+    `;
+    // Crear contenedor con toda la info del producto. Creamos un div.
+    const itemCarrito = document.createElement('div');
+
+    // Agregamos la clase al div
+    itemCarrito.classList.add('carrito__producto');
+
+    // Agregando plantilla al div
+    itemCarrito.innerHTML = plantillaProducto;
+
+    // Para agregarlo a nuestra pagina
+    // Esto es para agregar nuestro item al final de la lista.
+    ventanaCarrito.querySelector('.carrito__body').appendChild(itemCarrito);
+  });
 };
 
 // Abrir carrito
