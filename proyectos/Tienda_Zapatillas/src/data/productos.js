df7818d752f03275ce015d2f948d32c7a0cabe3a
José Tom´ás Otero García <otero.josetomas@gmail.com>
2023-08-21 11:28:49 -0400

    creando base de datos

---
+++ proyectos/Tienda_Zapatillas/src/data/productos.js
@@ -0,0 +1,23 @@
+export default {
+  productos: [
+    // Dentro de este objeto quiero guardar los productos. Producto 1
+    {
+      id: '1',
+      nombre: 'Tennis Converse Standard.',
+      descripcion: 'Tennis Converse unisex de calidad.',
+      precio: 500.0,
+      colores: ['negro', 'rojo', 'amarillo'],
+      tamaños: ['1.5', '2', '2.5', '3', '4'],
+    },
+
+    // Para agregar otro producto agregamos esto mismo.
+    {
+      id: '2',
+      nombre: 'Tennis Converse Pro.',
+      descripcion: 'Tennis Converse unisex de calidad pro.',
+      precio: 700.0,
+      colores: ['negro', 'rojo', 'amarillo'],
+      tamaños: ['1.5', '2', '2.5', '3', '4'],
+    }
+  ]
+}
