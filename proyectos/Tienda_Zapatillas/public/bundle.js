a2a2153bbf6ea16cfc62e8f20ddcdff57911fa9a
José Tom´ás Otero García <otero.josetomas@gmail.com>
2023-08-21 13:21:57 -0400

    agregando items al carrito

--- proyectos/Tienda_Zapatillas/public/bundle.js
+++ proyectos/Tienda_Zapatillas/public/bundle.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const producto = document.getElementById('producto');
-const productoImagen = producto.querySelector('.producto__imagen');
-const thumbs = producto.querySelector('.producto__thumbs');
+const producto$1 = document.getElementById('producto');
+const productoImagen = producto$1.querySelector('.producto__imagen');
+const thumbs = producto$1.querySelector('.producto__thumbs');
 
 // Accediendo propiedad color
 const propiedadColor = document.querySelector('#propiedad-color');
@@ -50,12 +50,20 @@ btnDisminuirCantidad.addEventListener('click', (e) => {
 const botonesAbrirCarrito = document.querySelectorAll('[data-accion="abrir-carrito"]');
 const botonesCerrarCarrito = document.querySelectorAll('[data-accion="cerrar-carrito"]');
 const ventanaCarrito = document.getElementById('carrito');
+// Detetar click en carrito
+const btnAgregarCarrito = document.getElementById('agregar-al-carrito');
+// accediendo a la variable del producto general, acceder al contenedor del producto
+const producto = document.getElementById('producto');
+// Arreglo vacio para gaurdar info de productos
+const carrito = [];
 
 /*
   Esta funcion se encargra de abrir el carrito y comprobar si hay o no productos en el carrito.
   Tambien la usaremos en varios bloques.
 */
 const renderCarrito =() => {
+
+  console.log(carrito);
 
   // Este modificador abriara la ventana.
   ventanaCarrito.classList.add('carrito--active');
@@ -76,5 +84,26 @@ botonesCerrarCarrito.forEach((boton) => {
 
     // Ahora hay que cerrarlo
     ventanaCarrito.classList.remove('carrito--active');
+  });
+});
+
+//Detectar click usuario
+btnAgregarCarrito.addEventListener ('click', (e) => {
+  // Accediendo a la info del producto, accediendo al id linea 57 index.html
+  const id = producto.dataset.productoId;
+  // tambien necesitamos el nombre, color, tamanio, etc.
+  const nombre = producto.querySelector('.producto__nombre').innerText;
+  const cantidad = parseInt(producto.querySelector('#cantidad').value);
+  // Hay que buscar el input que este marcado
+  const color = producto.querySelector('#propiedad-color input:checked').value;
+  const tamaño = parseInt(producto.querySelector('#propiedad-tamaño input:checked').value);
+
+  // Creamos un arreglo que va a contener todos los productos que el usuario agregue al carrito
+  carrito.push({
+    id: id,
+    nombre: nombre,
+    cantidad: cantidad,
+    color: color,
+    tamaño: tamaño,
   });
 });
--- proyectos/Tienda_Zapatillas/src/carrito.js
+++ proyectos/Tienda_Zapatillas/src/carrito.js
@@ -1,12 +1,20 @@
 const botonesAbrirCarrito = document.querySelectorAll('[data-accion="abrir-carrito"]');
 const botonesCerrarCarrito = document.querySelectorAll('[data-accion="cerrar-carrito"]');
 const ventanaCarrito = document.getElementById('carrito');
+// Detetar click en carrito
+const btnAgregarCarrito = document.getElementById('agregar-al-carrito');
+// accediendo a la variable del producto general, acceder al contenedor del producto
+const producto = document.getElementById('producto');
+// Arreglo vacio para gaurdar info de productos
+const carrito = [];
 
 /*
   Esta funcion se encargra de abrir el carrito y comprobar si hay o no productos en el carrito.
   Tambien la usaremos en varios bloques.
 */
 const renderCarrito =() => {
+
+  console.log(carrito);
 
   // Este modificador abriara la ventana.
   ventanaCarrito.classList.add('carrito--active');
@@ -27,5 +35,26 @@ botonesCerrarCarrito.forEach((boton) => {
 
     // Ahora hay que cerrarlo
     ventanaCarrito.classList.remove('carrito--active');
+  });
+});
+
+//Detectar click usuario
+btnAgregarCarrito.addEventListener ('click', (e) => {
+  // Accediendo a la info del producto, accediendo al id linea 57 index.html
+  const id = producto.dataset.productoId;
+  // tambien necesitamos el nombre, color, tamanio, etc.
+  const nombre = producto.querySelector('.producto__nombre').innerText;
+  const cantidad = parseInt(producto.querySelector('#cantidad').value);
+  // Hay que buscar el input que este marcado
+  const color = producto.querySelector('#propiedad-color input:checked').value;
+  const tamaño = parseInt(producto.querySelector('#propiedad-tamaño input:checked').value);
+
+  // Creamos un arreglo que va a contener todos los productos que el usuario agregue al carrito
+  carrito.push({
+    id: id,
+    nombre: nombre,
+    cantidad: cantidad,
+    color: color,
+    tamaño: tamaño,
   });
 });
