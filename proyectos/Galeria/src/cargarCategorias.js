7846b931c9081ca4ac8cbbb1088710a54a3c9c5b
José Tom´ás Otero García <otero.josetomas@gmail.com>
2023-08-24 12:50:37 -0400

    pequenios cambios en revision

--- proyectos/Galeria/src/cargarCategorias.js
+++ proyectos/Galeria/src/cargarCategorias.js
@@ -1,8 +1,10 @@
+// accediendo a mis categorias
 import dataCategorias from './datos/categorias';
 const { categorias } = dataCategorias;
 const contenedorCategorias = document.getElementById('categorias');
 
 categorias.forEach((categoria)=>{
+  // Queremos ver el codigo interno, el cual ya le agregaremos las categorias
   const nuevaCategoria = document.createElement('a');
   const plantilla = `
   <img class="categoria__img" src="${categoria.imagenPortada}" alt="" />
@@ -12,10 +14,12 @@ categorias.forEach((categoria)=>{
 						</div>
   `;
 
+  // Aqui agregamos a mano las categorias correspondientes.
   nuevaCategoria.innerHTML = plantilla;
   nuevaCategoria.classList.add('categoria');
   nuevaCategoria.href = '#';
   nuevaCategoria.dataset.categoria = categoria.id;
 
+  // Para guardar los datos de cada categoria
   contenedorCategorias.append(nuevaCategoria);
 });
