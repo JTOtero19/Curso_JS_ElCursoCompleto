b0f476b5938daa995b2ce16724f91776c7a31c53
José Tom´ás Otero García <otero.josetomas@gmail.com>
2023-09-26 15:28:35 -0300

    subidas generales

--- proyectos/App_Peliculas/src/index.js
+++ proyectos/App_Peliculas/src/index.js
@@ -1,6 +1,8 @@
 import cargarTitulos from "./cargarTitulos";
 import fetchPopulares from "./fetchPopulares";
 
+require('dotenv').config();
+
 const cargar = async () => {
   const resultados = await fetchPopulares();
   cargarTitulos(resultados);
